299695bb11f95b2fd55355b41fd02f5184f09a36
manudevelopia <manu@developia.info>
2022-07-20 19:13:18 +0200

    add Bowser and move it on the screen getting fury

--- go.mod
+++ go.mod
@@ -1,13 +1,15 @@
-module github.com/manudevelopia/gogame
+module github.com/manudevelopia/game-poc-go
 
-go 1.17
+go 1.18
 
 require (
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210410170116-ea3d685f79fb // indirect
-	github.com/hajimehoshi/ebiten/v2 v2.1.4 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220320163800-277f93cfa958 // indirect
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.3.6 // indirect
+	github.com/jezek/xgb v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
-	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb // indirect
-	golang.org/x/mobile v0.0.0-20210220033013-bdb1ca9a1e08 // indirect
+	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a // indirect
+	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
-	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
+	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
 )
--- main.go
+++ main.go
@@ -1,24 +1,61 @@
 package main
 
 import (
-	"log"
-
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	_ "image/png"
+	"log"
 )
 
-type Game struct{}
+var bowser_img *ebiten.Image
+var bowser_fury_img *ebiten.Image
+
+func init() {
+	var err error
+	bowser_img, _, err = ebitenutil.NewImageFromFile("bowser.png")
+	bowser_fury_img, _, err = ebitenutil.NewImageFromFile("bowser_fury.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Game struct {
+}
 
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		bowser_x += bowser_speed
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		bowser_x -= bowser_speed
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		bowser_y -= bowser_speed
+	} else if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		bowser_y += bowser_speed
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyA) {
+		bowser_fury = !bowser_fury
+	}
 	return nil
 }
 
+var bowser_x = 0
+var bowser_y = 0
+var bowser_speed = 3
+var bowser_fury = false
+
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello Óscar, this is your video game!")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(bowser_x), float64(bowser_y))
+
+	if bowser_fury == true {
+		screen.DrawImage(bowser_fury_img, op)
+	} else {
+		screen.DrawImage(bowser_img, op)
+	}
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
+	return 640, 480
 }
 
 func main() {
